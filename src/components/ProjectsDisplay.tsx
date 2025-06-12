3a4cf32c6df41e2609ff6efdcca7616e46689dc2
Yash Verma <recognizeyourself909@gmail.com>
2025-06-12 11:55:15 +0530

    update: projects array.

--- src/components/ProjectsDisplay.tsx
+++ src/components/ProjectsDisplay.tsx
@@ -4,13 +4,13 @@ import ProjectCard from './ProjectCard';
 import '@/styles/Projects.css'
 import projectsData from '@/data/projects.json'; // Importing the JSON data directly
 
-interface ProjectsDisplayProps {
-  // We might not need props if we directly import data,
-  // but could be useful for testing or if data source changes.
-  // projects: Project[]; 
-}
+// interface ProjectsDisplayProps {
+//   // We might not need props if we directly import data,
+//   // but could be useful for testing or if data source changes.
+//   // projects: Project[]; 
+// }
 
-const ProjectsDisplay: React.FC<ProjectsDisplayProps> = () => {
+const ProjectsDisplay: React.FC = () => {
   const projects: Project[] = projectsData; // Type assertion
 
   return (
