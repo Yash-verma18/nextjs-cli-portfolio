a8dd6d537e4f4ebc5e53fb28899c2f81ddd6c0e5
Yash Verma <recognizeyourself909@gmail.com>
2025-06-12 13:55:37 +0530

    card animations

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Project } from '@/types'; // Assuming your tsconfig paths are set up for @/
+import '@/styles/ScanHover.css';
+import '@/styles/CornerGlow.css';
 
 interface ProjectCardProps {
   project: Project;
@@ -7,29 +9,36 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
-    <div className="project-card">
-      {/* Border Elements */}
-      <span className="border-corner border-top-left">+</span>
-      <span className="border-corner border-top-right">+</span>
-      <span className="border-corner border-bottom-left">+</span>
-      <span className="border-corner border-bottom-right">+</span>
-      <span className="border-line border-top"></span>
-      <span className="border-line border-right"></span>
-      <span className="border-line border-bottom"></span>
-      <span className="border-line border-left"></span>
+    <div
+      className="scan-hover-wrapper"
+      style={{ '--scan-color': '#37F713' } as React.CSSProperties}
+    >
+      <div className="project-card social-link">
+        {/* Glowing Corner Brackets */}
+        <div className="corner top-left" />
+        <div className="corner top-right" />
+        <div className="corner bottom-left" />
+        <div className="corner bottom-right" />
 
-      {/* Actual Content */}
-      <div className="project-card-content">
-        <div className="project-card-title">
-          {project.titleLines.map((line, index) => (
-            <div key={index}>{line}</div>
-          ))}
-        </div>
-        <div className="project-card-separator">--------------------</div> {/* Placeholder for dashed line styling */}
-        <div className="project-card-description">
-          {project.descriptionLines.map((line, index) => (
-            <div key={index}>{line}</div>
-          ))}
+        {/* Old Border Elements */}
+        <span className="border-line border-top"></span>
+        <span className="border-line border-right"></span>
+        <span className="border-line border-bottom"></span>
+        <span className="border-line border-left"></span>
+
+        {/* Actual Content */}
+        <div className="project-card-content">
+          <div className="project-card-title">
+            {project.titleLines.map((line, index) => (
+              <div key={index}>{line}</div>
+            ))}
+          </div>
+          <div className="project-card-separator">--------------------</div>
+          <div className="project-card-description">
+            {project.descriptionLines.map((line, index) => (
+              <div key={index}>{line}</div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
